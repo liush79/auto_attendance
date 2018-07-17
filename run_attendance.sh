7fb75e02340f276c15358e22b376ab9bb159f665
seungho.ryu <98koom@hanmail.net>
2018-07-17 18:09:45 +0900

    Support to call from symbolic link

--- run_attendance.sh
+++ run_attendance.sh
@@ -1,3 +1,5 @@
-python ./src/kyobo.py
-python ./src/auction.py
+FULL_PATH=`readlink $0`
+SC_PATH=$(cd "$(dirname $FULL_PATH)" ; pwd -P)
+python $SC_PATH/src/kyobo.py
+python $SC_PATH/src/auction.py
 
