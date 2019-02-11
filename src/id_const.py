b44847388320a68d8468919c5fc05a6e55120ce4
seungho.ryu <98koom@hanmail.net>
2019-02-11 15:01:32 +0900

    Added id_cosnt_sample.py

--- src/id_const.py
+++ src/id_const.py
@@ -1,6 +1,9 @@
 import socket
 
-if 'ryu' in socket.gethostname():
+hostname = socket.gethostname()
+if 'ryu' in hostname:
     from id_const_ryu import *
+elif 'sample' in hostname:
+    from id_const_sample import *
 else:
     from id_const_for_commiting import *
---
+++ src/id_const_sample.py
@@ -0,0 +1,14 @@
+from id_const_for_commiting import *
+
+# Overriding part of IDs
+KYOBO['IDs'] = [
+    ['MyID_1', 'MyPassword_1'],
+]
+
+AUCTION['IDs'] = [
+    ['MyID_1', 'MyPassword_1'],
+]
+
+GMARKET['IDs'] = [
+    ['MyID_2', 'MyPassword_2'],
+]
